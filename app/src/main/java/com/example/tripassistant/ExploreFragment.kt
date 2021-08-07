6e27c54b090c9d1c25542ba7cf8aca8f9c248a62
Souptik-Coder <69388347+Souptik-Coder@users.noreply.github.com>
2021-08-07 19:35:48 +0530

    Set up navigation component with fragments

--- app/src/main/java/com/example/tripassistant/ExploreFragment.kt
+++ app/src/main/java/com/example/tripassistant/ExploreFragment.kt
@@ -5,6 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.FragmentNavigator
+import androidx.navigation.fragment.FragmentNavigatorExtras
+import androidx.navigation.fragment.findNavController
 import com.example.tripassistant.databinding.FragmentExploreBinding
 
 
@@ -17,9 +21,18 @@ class ExploreFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentExploreBinding.inflate(inflater, container, false)
+
+        binding.restaurant.setOnClickListener {
+            val action=ExploreFragmentDirections.actionGlobalSearchFragment("Find Restaurants in...")
+            findNavController().navigate(action)
+        }
+
+        binding.hotel.setOnClickListener {
+            val action=ExploreFragmentDirections.actionGlobalSearchFragment("Find Hotels in...")
+            findNavController().navigate(action)
+        }
+
+
         return binding.root
-
-
     }
-
 }
--- app/src/main/java/com/example/tripassistant/MainActivity.kt
+++ app/src/main/java/com/example/tripassistant/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.tripassistant
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.tripassistant.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.findNavController()
+
+        binding.bottomNav.setupWithNavController(navController)
     }
 }
